f0c990adab6f61702228d3877d104e07b686dda4
takbir <takbirulislam420@gmail.com>
2025-06-16 00:21:12 +0800

    add splash screen

--- lib/features/auth/ui/screen/splash_screen.dart
+++ lib/features/auth/ui/screen/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mkr_mart/features/auth/ui/widgets/app_logo.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,6 +13,22 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("data")));
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Spacer(),
+              AppLogo(width: 150),
+              Spacer(),
+              CircularProgressIndicator(),
+              SizedBox(height: 15),
+              Text("version: 1.0.0", style: TextStyle(color: Colors.grey)),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
